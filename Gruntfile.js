c3fab6c76435bea466708886bed3b25b93c0a0df
Mattia Belletti <mattia.belletti@gmail.com>
2015-04-28 17:32:50 +0200

    First commit

---
+++ Gruntfile.js
@@ -0,0 +1,106 @@
+module.exports = function(grunt) {
+
+    var tsServerSources = ['src/server/typescript/**/*.ts'];
+    var tsClientSources = ['src/client/typescript/**/*.ts'];
+    var staticDir = "src/client/static/";
+    var tsSources = tsServerSources.concat(tsClientSources);
+    var tsServerScript = 'build/server.js';
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        tslint: {
+            options: {
+                configuration: grunt.file.readJSON("tslint.json")
+            },
+            server: {
+                src: tsServerSources
+            },
+            client: {
+                src: tsClientSources
+            }
+        },
+        ts: {
+            server: {
+                src: tsServerSources,
+                out: tsServerScript,
+                options: {
+                    module: 'commonjs',
+                    fast: 'never'
+                }
+            },
+            client: {
+                src: tsClientSources,
+                out: 'build/client.js'
+            }
+        },
+        copy: {
+            statik: {
+                files: [
+                    {
+                        cwd: staticDir,
+                        src: ["**"],
+                        dest: "build/static",
+                        expand: true
+                    }
+                ]
+            }
+        },
+        watch: {
+            options: {
+                livereload: 11337
+            },
+            server: {
+                files: tsServerSources,
+                tasks: ['newer:tslint:server', 'ts:server'],
+                options: {
+                    spawn: false
+                }
+            },
+            client: {
+                files: tsClientSources,
+                tasks: ['newer:tslint:client', 'ts:client'],
+                options: {
+                    spawn: false
+                }
+            },
+            statik: {
+                files: staticDir + "/**",
+                tasks: ['newer:copy:statik'],
+                options: {
+                    spawn: false
+                }
+            }
+        },
+        nodemon: {
+            dev: {
+                script: tsServerScript
+            },
+            options: {
+                ignore: ['node_modules/**', 'Gruntfile.js'],
+                env: {
+                    port: '8181'
+                }
+            }
+        },
+        concurrent: {
+            watchers: {
+                tasks: ['nodemon', 'watch'],
+                options: {
+                    logConcurrentOutput: true
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks("grunt-newer");
+    grunt.loadNpmTasks('grunt-tslint');
+    grunt.loadNpmTasks('grunt-ts');
+    grunt.loadNpmTasks("grunt-contrib-watch");
+    grunt.loadNpmTasks("grunt-nodemon");
+    grunt.loadNpmTasks("grunt-concurrent");
+    grunt.loadNpmTasks("grunt-contrib-copy");
+
+    grunt.registerTask('default', ['tslint', 'ts', 'copy']);
+    grunt.registerTask('serve', ['default', 'concurrent:watchers']);
+};
---
+++ src/server/typescript/index.ts
@@ -0,0 +1,14 @@
+///<reference path="../../../typings/tsd.d.ts" />
+var connect = require("connect")
+    , serveStatic = require("serve-static")
+    , http = require("http")
+    , compression = require("compression")
+    ;
+
+var app = connect();
+app.use(serveStatic(__dirname + "/static", {
+    "index": ["index.html"]
+}));
+app.use(compression());
+
+http.createServer(app).listen(3000);
